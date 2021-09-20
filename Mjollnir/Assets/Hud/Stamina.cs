a253ad55a7675f8b59fad54ab793799dc23966a3
Jack <jacknm2020@gmail.com>
2021-09-20 11:46:50 -0700

    Initialize

---
+++ Mjollnir/Assets/Hud/Stamina.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class Stamina : MonoBehaviour
+{
+    static public Slider slider;
+    float currentStamina = 3f;
+    float maxStamina = 1000f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = currentStamina / maxStamina;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ReplenishStamina();
+        }
+    }
+
+    public void ReplenishStamina()
+    {
+        currentStamina += 5;
+    }
+}
---
+++ Mjollnir/Assets/Mjollnir/Scripts/MagicBoomerang.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicBoomerang : MonoBehaviour
+{
+    [SerializeField] Transform thrower;
+    public float duration;
+    [Range(50, 500)]
+    public float spinSpeed;
+    Coroutine throwCoroutine;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void ThrowBoomerang(Vector3 whereToGo)
+    {
+        throwCoroutine = StartCoroutine(Fly(whereToGo));
+    }
+
+    [ContextMenu("Throw To Origin")]
+    void ThrowBoomerang()
+    {
+        throwCoroutine = StartCoroutine(Fly(Vector3.zero));
+    }
+
+    IEnumerator Fly(Vector3 whereToGo)
+    {
+        Vector3 startPos = transform.position;
+        float startTime = Time.time;
+        transform.LookAt(whereToGo);
+        while(Time.time - startTime < duration)
+        {
+            transform.Rotate(Vector3.right, spinSpeed * Time.deltaTime, Space.Self);
+            transform.position = Vector3.Lerp(startPos, whereToGo, (Time.time - startTime) / duration);
+            yield return null;
+        }
+        startTime = Time.time;
+        while (Time.time - startTime < duration)
+        {
+            transform.Rotate(Vector3.left, spinSpeed * Time.deltaTime, Space.Self);
+            transform.position = Vector3.Lerp(whereToGo, thrower.transform.position, (Time.time - startTime) / duration);
+            yield return null;
+        }
+    }
+}
