364868bf7b4ab637f13195d74f4c7d5b66f52bc4
Jack <jacknm2020@gmail.com>
2021-10-04 12:49:48 -0700

    Waluigi

---
+++ Mjollnir/Assets/Movement/PlayerMovement2D.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float speed = 200;
+    Vector2 direction;
+    float jumpForce = 6f;
+    [SerializeField] GroundCheck checker;
+    Animator anim;
+    SpriteRenderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = new Vector2(Input.GetAxis("Horizontal"), 0);
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        anim.SetFloat("speed", Mathf.Abs(direction.x));
+        if (direction.x < 0)
+        {
+            rend.flipX = true;
+        } else
+        {
+            rend.flipX = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        anim.SetBool("onground", checker.onGround);
+    }
+
+    private void Move()
+    {
+        float vertical = rb.velocity.y;
+        float horizontal = direction.x * speed;
+        rb.velocity = new Vector2(horizontal, vertical); 
+    }
+
+    private void Jump()
+    {
+        if (checker.onGround)
+        {
+            anim.SetTrigger("jumping");
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
